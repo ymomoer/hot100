d67a6846b1b7e355a241f81ac13c1d35e5183171
mayuying <2995876255@qq.com>
2023-07-14 19:53:20 +0800

    update06

---
+++ 06-判断链表是否有环/快慢指针.js
@@ -0,0 +1,22 @@
+function ListNode(x) {
+  this.val = x;
+  this.next = null;
+}
+
+/**
+ *
+ * @param head ListNode类
+ * @return bool布尔型
+ */
+function hasCycle(head) {
+  // write code here
+  if (!head) return false;
+  let slow = head;
+  let fast = head.next;
+  while (fast && fast.next) {
+    if (slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return false;
+}
