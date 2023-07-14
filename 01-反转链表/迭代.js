7002d81ca6d2ce5da8aacc4fb3da813908b2bbd3
mayuying <2995876255@qq.com>
2023-07-14 19:35:59 +0800

    update01

---
+++ 01-反转链表/迭代.js
@@ -0,0 +1,22 @@
+function ListNode(x) {
+  this.val = x;
+  this.next = null;
+}
+
+function ReverseList(pHead) {
+  if (pHead === null || pHead.next === null) {
+    return pHead;
+  }
+  function ReverseList(pHead) {
+    if (pHead == null || pHead.next == null) return pHead;
+    let newHead = null;
+    cur = pHead;
+    while (cur != null) {
+      let temp = cur.next;
+      cur.next = newHead;
+      newHead = cur;
+      cur = temp;
+    }
+    return newHead;
+  }
+}
