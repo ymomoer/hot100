8734f32da26ab75ff5c4ca75736ffa6e12edebba
mayuying <2995876255@qq.com>
2023-07-14 23:24:12 +0800

    add 07-找到链表环的入口节点

---
+++ 07-找到链表环的入口节点/flag.js
@@ -0,0 +1,18 @@
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function EntryNodeOfLoop(pHead) {
+  // write code here
+  while (pHead) {
+    if (pHead.flag) {
+      return pHead;
+    } else {
+      pHead.flag = true;
+      pHead = pHead.next;
+    }
+  }
+}
+module.exports = {
+  EntryNodeOfLoop: EntryNodeOfLoop,
+};
