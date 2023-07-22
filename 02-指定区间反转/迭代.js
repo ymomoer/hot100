6c30e08e736491fb7745ea092322b58ff79724a1
mayuying <2995876255@qq.com>
2023-07-22 11:09:15 +0800

    02-指定区间反转

--- 02-指定区间反转/迭代.js
+++ 02-指定区间反转/迭代.js
@@ -1,27 +1,38 @@
-function ListNode(x) {
-  this.val = x;
-  this.next = null;
-}
-
+/*
+ * function ListNode(x){
+ *   this.val = x;
+ *   this.next = null;
+ * }
+ */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param head ListNode类
+ * @param m int整型
+ * @param n int整型
+ * @return ListNode类
+ */
 function reverseBetween(head, m, n) {
+  // write code here
   let dummyNode = new ListNode(-1);
-
   dummyNode.next = head;
-
-  let pre = dummyNode;
-  let cur = head;
+  pre = dummyNode;
+  cur = head;
 
   for (let i = 0; i < m - 1; i++) {
     pre = pre.next;
     cur = cur.next;
-
-    for (let i = 0; i < n - m; i++) {
-      let temp = cur.next;
-      cur.next = temp.next;
-      temp.next = pre.next;
-      pre.next = temp;
-    }
-
-    return dummyNode.next;
   }
+
+  for (let i = 0; i < n - m; i++) {
+    let temp = cur.next;
+    cur.next = temp.next;
+    temp.next = pre.next;
+    pre.next = temp;
+  }
+  return dummyNode.next;
 }
+module.exports = {
+  reverseBetween: reverseBetween,
+};
