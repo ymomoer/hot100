ae3e4241f186998ae174620455dc49e71bc92b00
mayuying <2995876255@qq.com>
2023-07-30 09:29:36 +0800

    25-二叉树的后序遍历

---
+++ 25-二叉树的后序遍历/递归.js
@@ -0,0 +1,29 @@
+/*
+ * function TreeNode(x) {
+ *   this.val = x;
+ *   this.left = null;
+ *   this.right = null;
+ * }
+ */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param root TreeNode类
+ * @return int整型一维数组
+ */
+function postorderTraversal(root) {
+  // write code here
+  let res = [];
+  function afterTraversal(root) {
+    if (root === null) return;
+    afterTraversal(root.left);
+    afterTraversal(root.right);
+    res.push(root.val);
+  }
+  afterTraversal(root);
+  return res;
+}
+module.exports = {
+  postorderTraversal: postorderTraversal,
+};
