6b21ab2b6047ac0ac73b95e4d46f4562665efeac
mayuying <2995876255@qq.com>
2023-08-28 16:44:38 +0800

    .\81-买卖股票的最好时机二\

---
+++ 81-买卖股票的最好时机二/动态规划.js
@@ -0,0 +1,26 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 计算最大收益
+ * @param prices int整型一维数组 股票每一天的价格
+ * @return int整型
+ */
+function maxProfit(prices) {
+  // write code here
+  const len = prices.length;
+  if (len < 2) {
+    return 0;
+  }
+  const dp = new Array(len).fill(0).map(() => new Array(2).fill(0));
+
+  dp[0][0] = 0;
+  dp[0][1] = -prices[0];
+  for (let i = 1; i < len; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+  }
+  return dp[len - 1][0];
+}
+module.exports = {
+  maxProfit: maxProfit,
+};
