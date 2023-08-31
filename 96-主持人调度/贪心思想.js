7727a06321557caa2fd78571a838cad4b8e1ebcf
mayuying <2995876255@qq.com>
2023-08-31 23:51:39 +0800

    .\96-主持人调度\

---
+++ 96-主持人调度/贪心思想.js
@@ -0,0 +1,35 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 计算成功举办活动需要多少名主持人
+ * @param n int整型 有n个活动
+ * @param startEnd int整型二维数组 startEnd[i][0]用于表示第i个活动的开始时间，startEnd[i][1]表示第i个活动的结束时间
+ * @return int整型
+ */
+function minmumNumberOfHost(n, startEnd) {
+  // write code here
+  let start = [];
+  let end = [];
+
+  for (let i = 0; i < n; i++) {
+    start.push(startEnd[i][0]);
+    end.push(startEnd[i][1]);
+  }
+  start.sort((a, b) => a - b);
+  end.sort((a, b) => a - b);
+
+  let res = 0;
+  let j = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (start[i] >= end[j]) {
+      j++;
+    } else {
+      res++;
+    }
+  }
+  return res;
+}
+module.exports = {
+  minmumNumberOfHost: minmumNumberOfHost,
+};
