40406e6ad3c9984fb3948cfcce2985dd9a6b7cd7
mayuying <2995876255@qq.com>
2023-09-02 16:34:36 +0800

    98-螺旋数组

--- 98-螺旋数组/边界模拟法.js
+++ 98-螺旋数组/边界模拟法.js
@@ -1,45 +1,42 @@
-/**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- *
- *
- * @param matrix int整型二维数组
- * @return int整型一维数组
- */
 function spiralOrder(matrix) {
   // write code here
-  let result = [];
   if (matrix.length === 0) {
-    return result;
+    return [];
   }
-  let left = 0;
-  let right = matrix[0].length - 1;
-  let up = 0;
-  let down = matrix.length - 1;
-  while (left <= right && up <= down) {
+  const res = [];
+  let top = 0,
+    bottom = matrix.length - 1,
+    left = 0,
+    right = matrix[0].length - 1;
+
+  while (top <= bottom && left <= right) {
+    // →
     for (let i = left; i <= right; i++) {
-      result.push(matrix[up][i]);
+      res.push(matrix[top][i]);
     }
-    up++;
-    for (let i = up; i <= down; i++) {
-      result.push(matrix[i][right]);
+    top++;
+    // ↓
+    for (let i = top; i <= bottom; i++) {
+      res.push(matrix[i][right]);
     }
     right--;
-    if (up <= down) {
-      for (let i = right; i >= left; i--) {
-        result.push(matrix[down][i]);
-      }
-      down--;
-    }
-    if (left <= right) {
-      for (let i = down; i >= up; i--) {
-        result.push(matrix[i][left]);
-      }
-      left++;
-    }
-  }
-  return result;
-}
 
+    // 如，[[2,3]]  要及时break
+    if (top > bottom || left > right) break;
+
+    // ←
+    for (let i = right; i >= left; i--) {
+      res.push(matrix[bottom][i]);
+    }
+    bottom--;
+    // ↑
+    for (let i = bottom; i >= top; i--) {
+      res.push(matrix[i][left]);
+    }
+    left++;
+  }
+  return res;
+}
 module.exports = {
   spiralOrder: spiralOrder,
 };
