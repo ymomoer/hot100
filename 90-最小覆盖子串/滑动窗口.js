ab23d0701b4a71de8ee8faf0b3d4360c83dcd81d
mayuying <2995876255@qq.com>
2023-09-03 10:06:16 +0800

    90-最小覆盖子串

--- 90-最小覆盖子串/滑动窗口.js
+++ 90-最小覆盖子串/滑动窗口.js
@@ -1,6 +1,4 @@
 /**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- *
  *
  * @param S string字符串
  * @param T string字符串
@@ -8,6 +6,50 @@
  */
 function minWindow(S, T) {
   // write code here
+  let Tmap = new Map();
+  let Rmap = new Map(); //用来和Tmap对比是否满足条件
+  let TLen = T.length;
+  let SLen = S.length;
+  let size;
+  let L = 0;
+  let R = 0;
+  let res = "";
+  for (var i = 0; i < TLen; i++) {
+    if (Tmap.has(T[i])) {
+      Tmap.set(T[i], Tmap.get(T[i]) + 1);
+    } else {
+      Tmap.set(T[i], 1);
+      Rmap.set(T[i], 0);
+    }
+  }
+  size = Tmap.size;
+  while (R < SLen) {
+    if (Rmap.has(S[R])) {
+      Rmap.set(S[R], Rmap.get(S[R]) + 1);
+      if (Rmap.get(S[R]) === Tmap.get(S[R])) {
+        size--;
+      }
+    }
+    while (size === 0) {
+      let newStr = S.slice(L, R + 1);
+      if (!res) {
+        res = newStr;
+      } else {
+        if (res.length > newStr.length) res = newStr;
+      }
+
+      if (Rmap.has(S[L])) {
+        Rmap.set(S[L], Rmap.get(S[L]) - 1);
+        if (Rmap.get(S[L]) < Tmap.get(S[L])) {
+          size++;
+        }
+      }
+      L++;
+    }
+
+    R++;
+  }
+  return res;
 }
 module.exports = {
   minWindow: minWindow,
